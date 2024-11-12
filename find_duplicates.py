e6739d93d252c2c834cd9efe8aed4d5432980ff0
ykpramodh <ykpramodh@gmail.com>
2024-11-12 09:28:36 +0530

    Update find_duplicates.py

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,4 +1,4 @@
-# python3 find_duplicates.py /Users/yk/toshiba rescan
+# python3 find_duplicates.py /path/to/directory rescan
 # python3 find_duplicates.py /path/to/directory rescan --min_size_mb 10
 # python3 find_duplicates.py /path/to/directory load_from_pickle
 # 
